a03650fee48e459cd645f186dbf83487c9c781f2
zhangqilin <zhangqilin@hungrypandagroup.com>
2021-08-16 20:49:46 +0800

    update desc

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
@@ -30,9 +29,11 @@ intellij {
 intellij {
     setPlugins("java", "android", "Kotlin")
 }
+
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
@@ -50,7 +51,8 @@ tasks {
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
     changeNotes(
         """
-      Add change notes here.<br>
-      <em>most HTML tags may be used</em>"""
+          update changeNotes
+           
+        """.trimIndent()
     )
 }
